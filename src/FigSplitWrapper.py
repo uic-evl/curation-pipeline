1d74ec38c45482dd6c6d09d15f2831ca6a12ca27
Juan <juan@10-8-231-211.vlan877.client.wireless.uic.edu>
2019-02-09 15:13:48 -0600

    FigSplit wrapper and test

---
+++ src/FigSplitWrapper.py
@@ -0,0 +1,60 @@
+from requests import post
+from os import listdir, remove
+from os.path import join
+from urllib import urlretrieve
+from sys import exc_info
+from zipfile import ZipFile
+
+FIGSPLIT_LOG_NAME = 'FigSplitLog.txt'
+
+
+class FigSplitWrapper:
+    def __init__(self, _service_url):
+        self.figsplit_url = _service_url
+
+    def split(self, _folder_path):
+        log_path = join(_folder_path, FIGSPLIT_LOG_NAME)
+        logfile = open(log_path, 'w')
+
+        figures = listdir(_folder_path)
+        total_figures = 0
+        successful_splits = 0
+
+        for figure in figures:
+            if figure.endswith((".jpg", ".png", ".jpeg", "bmp", "tif", ".tif")):
+                total_figures += 1
+                try:
+                    r = post("%s/modified_uploader" % self.figsplit_url, files={
+                        'file': open(join(_folder_path, figure), 'rb')
+                    })
+                    if r.status_code == 200:
+                        self.__download_splitted_content(_folder_path, r, figure)
+                        successful_splits += 1
+                    # else do what?
+                except Exception as e:
+                    print 'Unexpected error: %s' % exc_info()[0]
+                    logfile.write("%s: problem occurred\n%s" % (figure, e))
+
+        logfile.close()
+        return total_figures, successful_splits
+
+    def __download_splitted_content(self, _folder_path, _response, _figure_name):
+        html = _response.text.split('\n')
+        for line in html:
+            if 'download' in line and self.figsplit_url in line:
+                link_of_zip = line.split('href="')[1].split('" download')[0]
+                zip_name = '%s.zip' % _figure_name
+                zip_path = join(_folder_path, zip_name)
+                urlretrieve(link_of_zip, zip_path)
+                FigSplitWrapper.__unpackage_zip(_folder_path, zip_name)
+                remove(zip_path)
+
+    @staticmethod
+    def __unpackage_zip(_folder_path, zip_file):
+        path_to_zip = join(_folder_path, zip_file)
+        # zip files have this format: *.jpg.zip
+        zip_name = zip_file[:-8]
+
+        zip_ref = ZipFile(path_to_zip, 'r')
+        zip_ref.extractall(join(_folder_path, zip_name))
+        zip_ref.close()
---
+++ src/test/test_figsplit.py
@@ -0,0 +1,18 @@
+import sys
+from os import getcwd
+from os.path import abspath, join
+
+current_folder = getcwd()
+source_folder = abspath(join(current_folder, '..'))
+sys.path.append(source_folder)
+
+from FigSplitWrapper import FigSplitWrapper
+
+output_folder = abspath(join(current_folder, '..', '..', 'output'))
+input_folder_path = join(output_folder, '15350224')
+
+FIGSPLIT_URL = 'https://www.eecis.udel.edu/~compbio/FigSplit'
+fsw = FigSplitWrapper(FIGSPLIT_URL)
+
+total_figures, total_figures_splitted = fsw.split(input_folder_path)
+print "\nTotal Figures: %d\nTotal splitted: %d" % (total_figures, total_figures_splitted)

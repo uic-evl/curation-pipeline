ea366ab1b3fa3bb6048cd6a8c1116238183ce47a
Juan Trelles <juantrelles@gmail.com>
2020-12-08 21:59:19 -0500

    formatting

--- src/test/test_pdfigcapx.py
+++ src/test/test_pdfigcapx.py
@@ -5,7 +5,6 @@ from os.path import join, abspath
 current_folder = getcwd()
 source_folder = abspath(join(current_folder, '..'))
 sys.path.append(source_folder)
-
 from PDFigCapX import PDFigCapX
 
 chrome_driver_path = join('/usr/bin/chromedriver')
@@ -14,9 +13,9 @@ xpdf_pdftohtml_path = "/usr/local/bin/pdftohtml"
 input_path = abspath(join(current_folder, '..', '..', 'input'))
 output_path = '/mnt/output'
 
-p = PDFigCapX(
-    _chrome_drive_path=chrome_driver_path,
-    _xpdf_pdftohtml_path=xpdf_pdftohtml_path)
+p = PDFigCapX(_chrome_drive_path=chrome_driver_path,
+              _xpdf_pdftohtml_path=xpdf_pdftohtml_path)
 total_files, total_pdf, total_successes = p.extract(input_path, output_path)
 
-print("Total files: %d\nTotal PDFs: %d\nTotal successes: %d" % (total_files, total_pdf, total_successes))
+print("Total files: %d\nTotal PDFs: %d\nTotal successes: %d" %
+      (total_files, total_pdf, total_successes))

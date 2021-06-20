13fef1c7fc6d14ce10a928f79149c702f99fb8f5
Juan Trelles <juantrelles@gmail.com>
2021-06-20 15:47:13 -0500

    pdfigcapx parallel

--- src/core/PDFigCapX.py
+++ src/core/PDFigCapX.py
@@ -35,32 +35,35 @@ class PDFigCapX():
     """
 
     def __init__(self,
-                _dpi=300,
-                _chrome_drive_path='/usr/bin/chromedriver',
-                _xpdf_pdftohtml_path='/usr/local/bin/pdftohtml',
-                _imagemagick_convert_path=None):
+                 _dpi=300,
+                 _chrome_drive_path='/usr/bin/chromedriver',
+                 _xpdf_pdftohtml_path='/usr/local/bin/pdftohtml',
+                 _imagemagick_convert_path=None):
         self.chrome_driver_path = _chrome_drive_path
         self.xpdf_pdftohtml_path = _xpdf_pdftohtml_path
-        self.imagemagick_convert_path = _imagemagick_convert_path        
+        self.imagemagick_convert_path = _imagemagick_convert_path
         self.dpi = _dpi
         self.log_file = None
 
     def extract(self, _input_path, _output_path):
-        xpdf_output_path_prefix = join(_output_path, PDF_OUTPUT_FOLDER)  # /.../xpdf_
+        xpdf_output_path_prefix = join(
+            _output_path, PDF_OUTPUT_FOLDER)  # /.../xpdf_
         log_file_path = join(_output_path, LOG_FILE)
         total_extracted = 0
         total_pdfs = 0
         pdf_err_list = []
 
-        try :            
+        try:
             self.log_file = open(log_file_path, 'w')
-            pdfs = [x for x in listdir(_input_path) if x[0] != '_' and '.pdf' in x.lower() and os.path.isfile(join(_input_path, x))]
+            pdfs = [x for x in listdir(_input_path) if x[0] != '_' and '.pdf' in x.lower(
+            ) and os.path.isfile(join(_input_path, x))]
 
             for pdf in pdfs:
                 total_pdfs += 1
                 pdf_path = join(_input_path, pdf)
 
-                pil_images = renderer.render_pdf(pdf_path, self.imagemagick_convert_path, dpi=self.dpi)                    
+                pil_images = renderer.render_pdf(
+                    pdf_path, self.imagemagick_convert_path, dpi=self.dpi)
 
                 if self.__convert_pdf_to_html(xpdf_output_path_prefix, pdf, pdf_path):
                     if self.__process_figures(pil_images, _input_path, pdf, xpdf_output_path_prefix, _output_path):
@@ -69,17 +72,20 @@ class PDFigCapX():
                         pdf_err_list.append(pdf)
             return pdf_err_list, total_pdfs, total_extracted
         finally:
-            self.log_file.close()        
+            self.log_file.close()
 
     def __convert_pdf_to_html(self, _xpdf_output_path, _pdf, _pdf_path):
-        try:            
-            xpdf_pdf_path = _xpdf_output_path + _pdf[:-4]  # xpdf_{pdf_name_without_extension}
+        try:
+            # xpdf_{pdf_name_without_extension}
+            xpdf_pdf_path = _xpdf_output_path + _pdf[:-4]
             if not isdir(xpdf_pdf_path):
                 # execute xpdf if folder does not exist
-                std_out = subprocess.check_output([self.xpdf_pdftohtml_path, _pdf_path, xpdf_pdf_path])
+                std_out = subprocess.check_output(
+                    [self.xpdf_pdftohtml_path, _pdf_path, xpdf_pdf_path])
             return True
         except Exception as e:
-            self.log_file.write("ERROR: %s\t%s\n%s\n" % (_pdf, "xpdf_to_html", e))
+            self.log_file.write("ERROR: %s\t%s\n%s\n" %
+                                (_pdf, "xpdf_to_html", e))
             return False
 
     def py2_wrapper(self, input_path, pdf, xpdf_output_path, chromedriver):
@@ -94,11 +100,10 @@ class PDFigCapX():
                 channel.send(figures_captions_list(*channel.receive()))
                 """)
             channel.send([input_path, pdf, xpdf_output_path, chromedriver])
-            result = channel.receive()            
-            return result        
+            result = channel.receive()
+            return result
         finally:
             group.terminate(timeout=1.0)
-
 
     def __extract_figures(self, _input_path, _pdf, _xpdf_output_path):
         flag = 0
@@ -109,14 +114,16 @@ class PDFigCapX():
         while flag == 0 and wrong_count < MAX_WRONG_COUNT:
             try:
                 # process content using the ChromeDriver
-                figures, info = self.py2_wrapper(_input_path, _pdf, _xpdf_output_path, self.chrome_driver_path)
+                figures, info = self.py2_wrapper(
+                    _input_path, _pdf, _xpdf_output_path, self.chrome_driver_path)
                 flag = 1
             except Exception as e:
                 flag = 0
                 wrong_count += 1
                 time.sleep(5)
                 print("error _extract_figures")
-                self.log_file.write("ERROR: %s\t%s\n%s" % (_pdf, "_extract_figures (%d/%d)".format(wrong_count, MAX_WRONG_COUNT), e))
+                self.log_file.write("ERROR: %s\t%s\n%s" % (
+                    _pdf, "_extract_figures (%d/%d)".format(wrong_count, MAX_WRONG_COUNT), e))
 
             return figures, info, flag
 
---
+++ src/pdfigcapx.py
@@ -0,0 +1,98 @@
+from os.path import join, exists
+from core.PDFigCapX import PDFigCapX
+import concurrent.futures
+import logging
+import argparse
+
+chrome_driver_path = join('/usr/bin/chromedriver')
+xpdf_pdftohtml_path = "/usr/local/bin/pdftohtml"
+
+
+def setup_logger(logger_filepath):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+
+    file_handler = logging.FileHandler(logger_filepath)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def read_ids(filename):
+    try:
+        with open(filename, 'r') as file:
+            ids = file.read().splitlines()
+    except FileNotFoundError:
+        print(f"{filename} file not yet created")
+        ids = []
+    return ids
+
+
+def process_folder(_id, input_path, success_filepath, error_filepath,
+                   logger_filepath):
+    logger = logging.getLogger(logger_filepath)
+
+    input_folder_path = join(input_path, _id)
+
+    p = PDFigCapX(_chrome_drive_path=chrome_driver_path,
+                  _xpdf_pdftohtml_path=xpdf_pdftohtml_path)
+
+    pdfs_err_list, total_pdf, total_successes = p.extract(
+        input_folder_path, input_folder_path)
+    success = total_pdf == total_successes
+    output_filepath = success_filepath if success else error_filepath
+    with open(output_filepath, 'a') as file:
+        file.write("{0}\n".format(_id))
+    logger.info(f"{_id}\t {success}\t {total_pdf}\t {total_successes}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="batch pdfigcapx")
+    parser.add_argument('--input', type=str,
+                        help="folder containing folders to process")
+    parser.add_argument('--base_path', type=str,
+                        help="folder path for output ids")
+
+    args = parser.parse_args()
+
+    if not exists(args.input) or not exists(args.base_path):
+        print("File paths in arguments do not exists")
+        return
+
+    reprocess_filepath = join(args.base_path, "reprocess_pdfigcapx.txt")
+    success_filepath = join(args.base_path, "success_pdfigcapx.txt")
+    error_filepath = join(args.base_path, "error_pdfigcapx.txt")
+    logger_filepath = join(args.base_path, "logger_batch_pdfigcapx.log")
+
+    logger = setup_logger(logger_filepath)
+    reprocess_ids = read_ids(reprocess_filepath)
+    if len(reprocess_ids) == 0:
+        logger.error("File with ids to process is empty")
+        return
+    success_ids = read_ids(success_filepath)
+
+    ids = [x for x in reprocess_ids if x not in success_ids]
+    logger.info(f'Started processing {len(ids)} files')
+
+    try:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(process_folder, ids,
+                         [args.input] * len(ids),
+                         [success_filepath] * len(ids),
+                         [error_filepath] * len(ids),
+                         [logger_filepath] * len(ids))
+    except Exception:
+        logger.exception("exception caught while batch processing")
+    finally:
+        logger.info("finished batch processing")
+
+
+if __name__ == "__main__":
+    main()
